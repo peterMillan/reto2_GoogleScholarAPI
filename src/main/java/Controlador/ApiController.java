6bb69ce2fb4544ad5c4a83a50d82e66968bb1aa0
peterMillan <pedroutle@gmail.com>
2024-04-29 16:56:50 -0600

    V11

--- src/main/java/Controlador/ApiController.java
+++ src/main/java/Controlador/ApiController.java
@@ -37,4 +37,6 @@ public class ApiController {
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
     }
+
+    
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import Controlador.ApiController;
+import Vista.ApiView;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ApiController apiController = new ApiController();
+        ApiView mainView = new ApiView();
+
+        // Aquí proporciona la consulta y la clave de la API
+        String query = "Universidad del Norte de México";
+        String apiKey = "4f72ce14f0467b7efe3ec8648c5c854328331bd6af1119a9be68c6b40df39380";
+
+        try {
+            List<String> autores = apiController.obtenerAutores(query, apiKey);
+            mainView.mostrarAutores(autores);
+
+            // Contar la cantidad de artículos por autor
+            Map<String, Integer> conteoArticulos = apiController.contarArticulosPorAutor(autores);
+
+            // Obtener los 10 autores con más artículos
+            List<String> topAutores = apiController.obtenerTopAutores(conteoArticulos);
+            mainView.mostrarTopAutores(topAutores, conteoArticulos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
